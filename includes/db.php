ed5e082e744adbb43ffec846170736f336d88e64
Billy <hchoi92@hotmail.com>
2018-06-26 01:38:53 -0700

    reorganized code structure

--- includes/db.php
+++ includes/db.php
@@ -1,61 +1,64 @@
 <?php
 class dbManager {
 
-  var $dbServer;
-  var $dbLoginPW;
-  var $dbLoginID;
-  var $dbName;
-  var $conn;
+  static $dbServer;
+  static $dbLoginPW;
+  static $dbLoginID;
+  static $dbName;
+  static $conn;
+  static $isInit;
 
-  function __construct($dbServer, $dbLoginID, $dbLoginPW, $dbName){
+  public static function initialize($dbServer, $dbLoginID, $dbLoginPW, $dbName){
 
-    $this -> dbServer = $dbServer;
-    $this -> dbLoginID = $dbLoginID;
-    $this -> dbLoginPw = $dbLoginPW;
-    $this -> dbName = $dbName;
 
+    self::$dbServer = $dbServer;
+    self::$dbLoginID = $dbLoginID;
+    self::$dbLoginPW = $dbLoginPW;
+    self::$dbName = $dbName;
+    self::$isInit = 'True';
   }
 
-  function dbConnect(){
-    $this -> conn = mysqli_connect($this->dbServer, $this->dbLoginID, $this->dbLoginPW, $this->dbName);
+  public static function dbConnect(){
+    self::$conn = mysqli_connect(self::$dbServer, self::$dbLoginID, self::$dbLoginPW, self::$dbName);
   }
 
-  function fetchCategories(){
+  public static function fetchCategories(){
 
     $query = "SELECT * FROM categories";
-    return mysqli_query($this -> conn, $query);
+    return mysqli_query(self::$conn, $query);
 
   }
 
-  function insertCategory($cat_title){
+  public static function insertCategory($cat_title){
     $query = "INSERT INTO categories (cat_title) VALUES ('$cat_title')";
-    mysqli_query($this -> conn, $query);
+    mysqli_query(self::$conn, $query);
   }
 
-  function insertPost($post_title, $post_author, $post_date, $post_image){
+  public static  function insertPost($post_title, $post_author, $post_date, $post_image){
      // TODO: write this function
 
   }
 
-  function fetchPosts(){
+  public static function fetchPosts(){
 
       $query = "SELECT * FROM posts";
-      return mysqli_query($this -> conn, $query);
+      return mysqli_query(self::$conn, $query);
+
   }
 
   function fetchByUserSearch($tag){
 
     $query = "SELECT * FROM posts WHERE post_tags LIKE '%$tag%'";
-    return mysqli_query($this -> conn, $query);
-
+    return mysqli_query(self::$conn, $query);
   }
 
+  public static function checkIfSet_POST($name){
+
+  if (isset($_POST[$name]))
+    return true;
+  return false;
+
+
+  }
 }
-
-
-
-
-
-
-
 ?>
